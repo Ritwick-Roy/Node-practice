0a363fdb21091d921f9cd9a1c710ff51c46bffb0
Ritwick-Roy <ritwickroy2002@gmail.com>
2021-09-03 00:39:37 +0530

    Initial commit

--- module.js
+++
@@ -1,8 +0,0 @@
-//program to use objects from other programs
-
-const xyz=require('./people');
-
-console.log(xyz);
-
-const os=require("os");
-console.log(os.platform(),os.homedir());
--- people.js
+++
@@ -1,8 +0,0 @@
-//this file stores some arrays that we will use in a different program
-
-const name=["abc","rishi","noob"]
-const ages=["12","19","20"]
-console.log(name);
-module.exports={
-    name,ages
-};
